84b5280d40e979098b7beee3b3e3759f7f91e8e0
Thanawit Gerdprasert <thanawit.g@ku.th>
2017-07-03 09:06:56 +0900

    changed some file

--- tags_mining.js
+++ tags_mining.js
@@ -6,10 +6,10 @@ var fs = require('fs');
 var geoUser = [];
 
 var T = new Twit({
-  consumer_key: 'qcUO2JG4GVMHjBKwO8dPHSaEk',
-  consumer_secret: '2phK6LFhIdQ1CVUxkGuymJl4jJv0L2ML7Wi1ZcGBcEURr2cCmB',
-  access_token: '183192102-8XCt3EIBATFUtIcszr89juvbrPxwsvGMTzqEmIC5',
-  access_token_secret: 'bUZdOIwndhk7xDVlFluqAnfVFOahoYmndG6qFmkZtouSW'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 });//insert your config here
 
 var currentID ;
